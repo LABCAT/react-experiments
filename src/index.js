e873ab9cd71323b3c78525564e7a479daeeb8b2d
LABCAT <shane.watters.nz@gmail.com>
2020-04-04 17:14:13 +1300

    Completing basic intro to React Router tutorial.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './scss/main.scss';
 import Main from './js/Main';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('main'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Main />
+    </BrowserRouter>, 
+    document.getElementById('main')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/js/Main.js
+++ src/js/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
+
+import Navbar from "./layout/Navbar";
+
 import Home from './page-types/Home';
+import About from "./page-types/About";
+import Contact from "./page-types/Contact";
 
 function Main() {
   return (
-    <Home/>
+    <main>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </main>
   );
 }
 
